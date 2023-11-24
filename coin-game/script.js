793531c42c8bcd9f7a734242f5e1190813f2369c
Gabriela Farinha <vallefarinha@gmail.com>
2023-11-24 10:39:20 -0300

    adding new styles

---
+++ coin-game/script.js
@@ -0,0 +1,17 @@
+/*
+botao de girar moeda
+botao de iniciar jogo
+
+___________________________
+
+contador cara
+contador coroa
+
+*/
+
+document.addEventListener('click', function (event) {
+   
+
+
+
+});
