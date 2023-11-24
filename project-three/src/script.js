9361d00752665698ca07078f7c167a99404ce987
Gabriela Farinha <vallefarinha@gmail.com>
2023-11-24 07:46:01 -0300

    calculator working

--- project-three/src/script.js
+++ project-three/src/script.js
@@ -1,71 +1,58 @@
+let resultContainer = document.querySelector("#result");
+let previousNumber = document.querySelector("#previous-number");
 
-    let resultContainer = document.querySelector("#result");
-    let previousNumber = document.querySelector("#previous-number")
+function printFirstNumber(event) {
+    let buttonValue = event.target.textContent;
+    resultContainer.value += buttonValue;
+    return resultContainer.value;
+    
+}
 
-   let numbers=[]; 
-   
-    function printFirstNumber(event) {
-        let buttonValue = event.target.textContent;
-        resultContainer.value += buttonValue;
-        let firstNumber = resultContainer.value;
-        return firstNumber;
-
-            // if (event.target.classList.contains('btn-operation')){
-            // let operator = 
-            // firstNumber = firstNumber.slice(0, -1);
-        
-            // //         numbers.push(content.charAt(content.length - 1));
-            // }           
-        }
-      
-
-
-    // function printSecondNumber() {
-    //     if (event.target.classList.contains('btn-input')) {
-    //         let buttonValue = event.target.textContent;
-    //         resultContainer.textContent += buttonValue;
-    //     }
-    // }
-
-    // function operationButton(){
-    //     if (event.target.classList.contains('btn-operation')){
-    //         let content = resultContainer.textContent;         
-    //         numbers.push(content.slice(0,-1));
-    //         numbers.push(content.charAt(content.length - 1));
-    //         resultContainer.textContent = "";
-    //         console.log(numbers);
-    //         printFirstNumber();
-      
-    // }
-
-    // }
-
-
-
+document.addEventListener('click', function (event) {
   
-    document.addEventListener('click', function (event) {      
+    if (event.target.classList.contains('btn-input')) {
         printFirstNumber(event);
+    }
+
+    if (event.target.classList.contains('btn-operation')) {
+        previousNumber.value =  printFirstNumber(event);
+        resultContainer.value = "";
+    }
+
+    if (event.target.classList.contains('equal')) {
+      let operation = previousNumber.value += printFirstNumber(event);
+      previousNumber.value = operation;
+      operation = operation.slice(0, -1);
+      console.log(operation);
+      resultContainer.value = eval(operation);
       
-        if (event.target.classList.contains('equal')){
-            resultContainer.textContent = eval(printFirstNumber);
+     
+    }
 
-        }
+    if (event.target.classList.contains('clearAll')) {
+        resultContainer.value = "";
+        previousNumber.value = "";
+    }
 
-        if (event.target.classList.contains('clearAll')){
-            resultContainer.value = "";
-        }
+    if (event.target.classList.contains('delete')) {
+        resultContainer.value = resultContainer.value.slice(0, -3);
+    }
+});
 
-        if (event.target.classList.contains('delete')){
-            resultContainer.value = resultContainer.value.slice(0, -3);
-
-        }
-   }
-);
 
 // function sum(){
-//         let sum = 0
-//         for(i = 0; i < numbers.length; i++){
-//         sum += numbers[i];
-//         }
+
 // }
 
+
+// function sub(){
+    
+// }
+
+// function divide(){
+    
+// }
+
+// function multiply(){
+    
+// }
