9e13771fa71b4fd9d2a185ec9f962afb3d9d5108
Gabriela Farinha <vallefarinha@gmail.com>
2023-12-07 15:40:48 -0300

    adding coin-game

--- coin-game/script.js
+++ coin-game/script.js
@@ -1,17 +1,47 @@
-/*
-botao de girar moeda
-botao de iniciar jogo
+let counter = document.querySelectorAll(".counter");
+let restartBtn = document.getElementById("restart-btn");
+let images = document.getElementById("container-images");
+let turnBtn = document.getElementById("turn-btn");
+let headImg = document.getElementById("head");
+let tailImg = document.getElementById("tail");
+let tailCounter = document.getElementById("tail-counter");
+let headCounter = document.getElementById("head-counter");
 
-___________________________
+let isFlipping = false;
 
-contador cara
-contador coroa
+let heads = 0;
+let tails = 0;
 
-*/
+turnBtn.addEventListener("click", () =>{
+    let i = Math.floor(Math.random()*2);
+    images.style.animation = "none";
 
-document.addEventListener('click', function (event) {
-   
+    if(i){
+        setTimeout(function(){
+            images.style.animation = "spin-heads 3s forwards"
+        }, 100);
+        heads++
+    }
+    else {
+        setTimeout(function(){
+            images.style.animation = "spin-tails 3s forwards"
+        }, 100);
+        tails++
+    }
+
+    setTimeout(updateStats,3000);
+});
 
 
+function updateStats(){
+    headCounter.textContent = heads;
+    tailCounter.textContent = tails;
+}
 
+
+restartBtn.addEventListener("click", () =>{
+    images.style.animation = "none";
+    heads = 0;
+    tails=0;
+    updateStats();
 });
